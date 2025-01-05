32137e884416565f216ab07a39a96f07403d659e
GCPeng48 <hokanki33@gmail.com>
2025-01-05 20:14:05 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ const path = require('path')
 export default {
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: './dist', // 保持輸出目錄為相對根目錄的 dist
+    emptyOutDir: true, // 每次構建清空輸出目錄
   },
   server: {
     port: 8080
